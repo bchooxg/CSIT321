3c44e7ae66c7bb592cdf97011dbc57d25984c205
Razshort <zhiyang823@gmail.com>
2022-12-12 21:02:20 +0800

    Login page + settings (zhiyang)

--- app/src/main/java/com/example/securefolder/MainActivity.java
+++ app/src/main/java/com/example/securefolder/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
